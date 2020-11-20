449c9a75b55124f918059104df72815a2185c4d1
Ollie Taylor <contact@olliejt.com>
2020-11-20 20:11:06 +0100

    Update 2-install-additional.sh

--- home/.scripts/2-install-additional.sh
+++ home/.scripts/2-install-additional.sh
@@ -4,8 +4,9 @@
 # Enabling all repositories and refreshing the software list
 # ==========
 
-sudo add-apt-repository -y universe # For Ubuntu
-sudo add-apt-repository -y ppa:deadsnakes/ppa # For Python
+sudo add-apt-repository -y ppa:agornostal/ulauncher
+sudo add-apt-repository -y ppa:teejee2008/timeshift
+
 
 sudo apt-get update
 sudo apt-get upgrade -y
@@ -18,19 +19,15 @@ sudo apt-get dist-upgrade
 # REMOVE = sudo apt-get remove {package_name}
 # ==========
 
-sudo apt install preload bleachbit ulauncher gnome-tweak-tool chrome-gnome-shell chromium-browser -y
+sudo apt install preload bleachbit ulauncher gnome-tweak-tool chrome-gnome-shell chromium-browser flatpak -y
 
-sudo snap install spotify discord whatsdesk hey-mail postman beekeeper-studio insomnia
-### todo: update to stable when launched
-sudo snap install 1password --edge
+sudo snap install whatsdesk hey-mail beekeeper-studio insomnia
+sudo snap install 1password --edge ### todo: update to stable when launched
 
-### Installing OMGZSH
-chsh -s /usr/bin/zsh
-sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Flatpaks
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-
+sudo flatpak flatpack com.spotify.Client com.dropbox.Client com.discordapp.Discord  com.getpostman.Postman -y
 
 # ==========
 # Installing Python Packages
@@ -54,26 +51,9 @@ pip3 install pygithub
 # sudo dpkg -i package_name.deb
 # ==========
 
-echo "
-	$fg_bold[white]v^v^v^v^v^v^
-	$fg_bold[yellow]--> $fg_bold[red]MANUAL INSTALL DROPBOX
-	$fg_bold[yellow]--> $fg_bold[white]Save to /Downloads: $fg_bold[yellow]https://www.dropbox.com/install-linux
-	$fg_bold[yellow]--> $fg_bold[green]sudo dpkg -i ~/Downloads/dropbox_2020.03.04_amd64.deb
-	$fg_bold[white]v^v^v^v^v^v^
-"
 
-echo "
-	$fg_bold[red]v^v^v^v^v^v^
-	$fg_bold[yellow]--> $fg_bold[red]MANUAL INSTALL GITHUB DESKTOP
-	$fg_bold[yellow]--> $fg_bold[red]Save to /Downloads: $fg_bold[yellow]https://github.com/shiftkey/desktop/releases
-	$fg_bold[yellow]--> $fg_bold[green]sudo dpkg -i ~/Downloads/GitHubDesktop-linux-2.5.7-linux1.deb
-	$fg_bold[red]v^v^v^v^v^v^
-"
-
-echo "
-	$fg_bold[red]v^v^v^v^v^v^
-	$fg_bold[yellow]--> $fg_bold[red]MANUAL INSTALL FLATPACK
-	$fg_bold[yellow]--> $fg_bold[red]Save to /Downloads: $fg_bold[yellow]https://flatpak.org/setup/Ubuntu/
-	$fg_bold[yellow]--> $fg_bold[green]Don't forget flathub
-	$fg_bold[red]v^v^v^v^v^v^
-"
+# echo "$fg_bold[red]v^v^v^v^v^v^
+# $fg_bold[yellow]--> $fg_bold[red]MANUAL INSTALL GITHUB DESKTOP
+# $fg_bold[yellow]--> $fg_bold[red]Save to /Downloads: $fg_bold[yellow]https://github.com/shiftkey/desktop/releases
+# $fg_bold[yellow]--> $fg_bold[green]sudo dpkg -i ~/Downloads/GitHubDesktop-linux-2.5.7-linux1.deb
+# $fg_bold[red]v^v^v^v^v^v^"
